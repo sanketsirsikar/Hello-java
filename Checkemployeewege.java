c7cbb90502d800b3593d575779521e7de116690f
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-08-16 10:43:15 +0530

    initial commit

---
+++ Checkemployeewege.java
@@ -0,0 +1,13 @@
+package Javapratice;
+
+public class Checkemployeewege {
+public static void main (String args[]) {
+double empcheck = Math.floor(Math.random()*10%2);
+if (empcheck == 1) {
+	System.out.println("emplyee is present");
+}else {
+	System.out.println("emplyee is absent");
+	
+}
+}
+}
